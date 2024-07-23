5e6a63bb814b6847a6359199798b405846f17051
FTMustafa <mustafamustafatw@gmail.com>
2024-07-24 01:16:57 +0300

    .
    API ve kategoriye göre arama eklendi

--- src/components/pages/Recipes.js
+++ src/components/pages/Recipes.js
@@ -1,19 +1,47 @@
 import "./Recipes.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Recipes() {
+  const [arama, setArama] = useState("");
+  const [veri, setVeri] = useState([]);
+
+  useEffect(() => {
+    const veriCek = async () => {
+      try {
+        const response = await axios.get(
+          "https://www.themealdb.com/api/json/v1/1/categories.php"
+        );
+        console.log(response.data);
+        setVeri(response.data.categories); // Burada categories dizisini setVeri'ye atıyoruz
+      } catch (err) {
+        console.error("Veri çekme sırasında bir hata oluştu:", err);
+      }
+    };
+
+    veriCek();
+  }, []);
+
+  const filteredData = veri.filter((item) =>
+    item.strCategory.toLowerCase().includes(arama.toLowerCase())
+  );
+
   return (
     <div className="recipes">
-      <div>
-        <div className="card-section">
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-        </div>
+      <input
+        type="text"
+        placeholder="Yemek tarifi arama.."
+        value={arama}
+        onChange={(e) => setArama(e.target.value)}
+        className="search-bar"
+      />
+      <div className="card-section">
+        {filteredData.map((item) => (
+          <div key={item.idCategory} className="card">
+            <img src={item.strCategoryThumb}/>
+            {item.strCategory}
+          </div>
+        ))}
       </div>
     </div>
   );
